fcb6bcbfa10e2a7f346d4712e7d89a95f0bc4e16
Guedes <pedrogg@id.uff.br>
2024-05-20 21:04:43 -0300

    comentarios add

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
+# O seguinte código apresenta o corpo principal do jogo "King Pong"
+# feito para o projeto de Laboratório de Jogos.
+
+
 from PPlay.window import *
 from PPlay.sprite import *
 from PPlay.gameimage import *
 from PPlay.gameobject import *
 import os
 
+#Cria o diretório do arquivo principal. Garante compatibilidade com MAC e LINUX
 project_directory = os.path.dirname(__file__)
 
-# Janelas
+# Informações e Criação da Janela
 Screen_H, Screen_W = 720, 1280
 janela = Window(Screen_W,Screen_H)
 janela.set_title('King Pong')
-
-
 
 
 # Mouse e Teclado
 mouse = Window.get_mouse()
 teclado = Window.get_keyboard()
 
-#Dificuldade
+#Dificuldade (no shit)
 dificuldade = 0
+
+
 
 #Telas Iniciais do Game
 def gameINIT():
+
     #Tela "A Game Lab Project"
     image_path_GLP = os.path.join(project_directory, "Sprites", "aGameLabProject.png")
     tittleScreen_1 = GameImage(image_path_GLP)
@@ -33,79 +39,81 @@ def gameINIT():
 
     while True:
 
-        #Loop da priemira tela
+        #LCountDown da priemira tela
         time = 15
         while time > 0:
             time -= 10 * janela.delta_time()
             tittleScreen_1.draw()
             janela.update()
 
-        #Loop da Segunda tela
+        #CountDown da segunda tela
         time = 15
         while time > 0:
             time -= 10 * janela.delta_time()
             tittleScreen_2.draw()
             janela.update()
 
-        #Inicia o Menu
-        menu()
+        
+        menu()                          #Inicia o Menu
 
+# Menu Principal
 def menu():
 
-    #Inicializa Imagens usadas no Menu
+    # Inicializa Imagens usadas no Menu
 
+    # Imagem que aparece junto com o "Press Space to Begin"
     image_path_fundo = os.path.join(project_directory, "Sprites", "menuScreen.png")
     Fundo = GameImage(image_path_fundo)
 
+    # Imagem conjunta do botão "Play", "Diff" e "Quit"
     image_path_Menus = os.path.join(project_directory, "Sprites", "menuSelector.png")
     Menus = GameImage(image_path_Menus)
-    
+
+    # Imagem do pixel de seleção do menu principal
     image_path_Indicador = os.path.join(project_directory, "Sprites", "Indicador.png")
     Indicador = GameImage(image_path_Indicador)
 
 
-
+    #Variavel que confere se o jogador apertou espaço para entrar no menu
     Started = False
 
 
     while True:
 
-        while Started == False:
+        
+        while Started == False:     # Mini Loop que pede para o jogador apertar espaço
 
             Fundo.draw()
+
             janela.draw_text(text="- Press Space to Begin -", x= Screen_W/2 + 200,
                               y= Screen_H/2 + 100, size= 30, color=(255,255,255), font_name="Arial")
+            
             if teclado.key_pressed("SPACE"):
                 Started = True
 
             janela.update()
+        
 
-
-        janela.set_background_color([0,0,0])
+        #Começo do Loop do Menu 
+        janela.set_background_color([0,0,0]) 
         Menus.draw()
 
 
         #As seguintes linhas são em relação aos clicks do mouse 
-        
-                #Botão de Play
-        if mouse.is_over_area([455, 280], [760, 340]):
+                
+        if mouse.is_over_area([455, 280], [760, 340]):          #Botão de Play
             Indicador.set_position(400, 300)
             Indicador.draw()
-            
             if mouse.is_button_pressed(1):
                 play()
-            
-                #Botão de Dificuldades
-        if mouse.is_over_area([455, 380], [760, 440]):
+                
+        if mouse.is_over_area([455, 380], [760, 440]):          #Botão de Dificuldades
             Indicador.set_position(400, 400)
             Indicador.draw()
-            
             if mouse.is_button_pressed(1):
                 dificuldades()
-            
-
-                #Botão de Sair
-        if mouse.is_over_area([455, 480], [760, 540]):
+                
+        if mouse.is_over_area([455, 480], [760, 540]):          #Botão de Sair
             Indicador.set_position(400, 500)
             Indicador.draw()
 
