81dc9a4dee4d1aca94c0b0245f13c5b4463b12c2
Guedes <pedrogg@id.uff.br>
2024-07-07 21:21:21 -0300

    animations finish

--- animations.py
+++ animations.py
@@ -23,11 +23,31 @@ def passou_fase(project_directory,janela, N_Fase):
         Torre.set_position(0, -720)
         PlayerHead.set_position(850, 555)
         nivel_a_chegar = 360
+        nivel_a_chegar_torre = -720
 
     if N_Fase == 1:
         Torre.set_position(0, -720)
         PlayerHead.set_position(850, 360)
-
+        nivel_a_chegar = 165
+        nivel_a_chegar_torre = -720
+    
+    if N_Fase == 2:
+        Torre.set_position(0, -720)
+        PlayerHead.set_position(850, 165)
+        nivel_a_chegar = 170
+        nivel_a_chegar_torre = -510
+    
+    if N_Fase == 3:
+        Torre.set_position(0, -510)
+        PlayerHead.set_position(850, 165)
+        nivel_a_chegar = 170
+        nivel_a_chegar_torre = -320
+    
+    if N_Fase == 4:
+        Torre.set_position(0, -320)
+        PlayerHead.set_position(850, 165)
+        nivel_a_chegar = 170
+        nivel_a_chegar_torre = -0
 
 
     #ENEMMY FELLEDDD!!!!!!!!!
@@ -46,6 +66,9 @@ def passou_fase(project_directory,janela, N_Fase):
             aceleration -= 10 * janela.delta_time()
         if PlayerHead.y > nivel_a_chegar:
             PlayerHead.y -= (10 + aceleration)* janela.delta_time()
+        if Torre.y < nivel_a_chegar_torre:
+            Torre.y += (10 + aceleration) *janela.delta_time()
+        
 
 
         Torre.draw()
